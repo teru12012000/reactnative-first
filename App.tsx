ddbdda06d487ae8852e31a809617ef749d80c9bf
teru12012000 <sion0012@outlook.jp>
2023-04-09 23:38:20 +0900

    count app

--- App.tsx
+++ App.tsx
@@ -1,10 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [count,setCount]=useState<number>(0);
+  const handleinclease=()=>{
+    setCount(count+1);
+  }
+  const handledecrease=()=>{
+    if(count===0){
+      setCount(0)
+    }else{
+      setCount(count-1);
+    }
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.text}>Count</Text>
+      <Text>{count}</Text>
+      <View style={styles.btnstyle}>
+          <Button title='+' onPress={()=>handleinclease()} color="white"/>
+      </View>
+      <View style={styles.btnstyle}>
+        <Button title='-' onPress={()=>handledecrease()} color="white"/>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +32,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text:{
+    fontSize:30,
+  },
+  btnstyle:{
+    marginTop:10,
+    backgroundColor:"aqua",
+    borderRadius:50,
+    width:40,
+    height:40,
   },
 });
